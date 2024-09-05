5a99b800f73ed22ae469082ec6303e1c2e5a027b
ha1dysh <olekshaid@gmail.com>
2024-09-05 21:58:55 +0300

    translate meta title

--- source/app/.server/admin/loaders/dashboard.loader.ts
+++ source/app/.server/admin/loaders/dashboard.loader.ts
@@ -1,9 +1,13 @@
 import {json, LoaderFunctionArgs} from '@remix-run/node';
 import {getAuthUser} from '~/.server/admin/services/auth.service';
 import {userMapper} from '~/.server/admin/mappers/user.mapper';
+import i18nServer from '~/locales/i18n.server';
 
 export async function adminDashboardLoader({request}: LoaderFunctionArgs) {
   const user = await getAuthUser(request);
 
-  return json({user: userMapper(user)});
+  const t = await i18nServer.getFixedT(request);
+  const title = t('dashboard.meta.title');
+
+  return json({ user: userMapper(user), meta: { title } });
 }
